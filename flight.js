db190715a75ce663b2ea5b8a538e5edbde2c3584
jessiichat <jessiichat@gmail.com>
2021-01-05 10:09:25 +0100

    first commit

---
+++ flight.js
@@ -0,0 +1,80 @@
+const vols = [{
+  destination: 'Martinique',
+  date_depart: '01/25/2019',
+  compagnie_aerienne: 'Ryanair',
+  numVol: 'xxxxx',
+  prix: '350',
+  dureeTrajet: '6'
+},
+{
+  destination: 'Londres',
+  date_depart: '07/25/2019',
+  compagnie_aerienne: 'Air france',
+  numVol: 'xxxxx',
+  prix: '150',
+  dureeTrajet: '2'
+},
+{
+  destination: 'Paris',
+  date_depart: '08/12/2019',
+  compagnie_aerienne: 'Air france',
+  numVol: 'xxxxx',
+  prix: '50',
+  dureeTrajet: '1'
+},
+{
+  destination: 'Mexico',
+  date_depart: '09/03/2019',
+  compagnie_aerienne: 'Lufthansa',
+  numVol: 'xxxxx',
+  prix: '1350',
+  dureeTrajet: '8'
+},
+{
+  destination: 'Tokyo',
+  date_depart: '12/12/2020',
+  compagnie_aerienne: 'Air france',
+  numVol: 'xxxxx',
+  prix: '750',
+  dureeTrajet: '14'
+},
+{
+  destination: 'Tokyo',
+  date_depart: '12/11/2020',
+  compagnie_aerienne: 'Lufthansa',
+  numVol: 'xxxxx',
+  prix: '850',
+  dureeTrajet: '14'
+}
+];
+
+
+//Affichez tous les vols (destination - Date de départ- compagnie aérienne -
+//numero de vol - prix - durée du trajet)
+
+
+
+//Afficher tous les vols dont le trajet est inférieur à 7h. (idem)
+
+
+//Afficher les vols à destination de Tokyo et qui voyagent avec Air france (idem)
+
+
+
+//Afficher tous les vols dont le tarif est inférieur à 700 euros et qui
+//voyagent avec Air france
+
+
+
+//Afficher tous les vols qui voyagent avec 'Lufthansa' dont le prix est inférieur à 900 euros
+//et dont la durée du trajet est inférieur à 15 heures
+
+
+
+//Appliquer une reduc de 20% sur chaque vol à destination de Tokyo.
+
+
+
+
+//Allez voir ce qu'est un JSON ;)
+//https://developer.mozilla.org/fr/docs/Learn/JavaScript/Objects/JSON
